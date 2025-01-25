5fb6e4aad25e535cde7e6db95e269ff91dadd9a2
Fabian Tamp <fabian.tamp@gmail.com>
2025-01-25 15:26:56 +0100

    Code to stuff multiple files into the same image, notes on Metadata

--- src/assemble/main.rs
+++ src/assemble/main.rs
@@ -1,9 +1,8 @@
 use image::GenericImageView;
 use libheif_rs::{Channel, ColorSpace, CompressionFormat, EncoderQuality, HeifContext, Image, LibHeif, RgbChroma};
 
-
-fn encode2() -> libheif_rs::Result<()> {
-    let source = image::open("output/win98_00_00.png").unwrap();
+fn frame_from_path(path: &str) -> libheif_rs::Result<Image> {
+    let source = image::open(&path).unwrap();
 
     let width = source.width();
     let height = source.height();
@@ -37,7 +36,10 @@ fn encode2() -> libheif_rs::Result<()> {
             pixel_index += 1;
         }
     }
+    Ok(image)
+}
 
+fn encode2() -> libheif_rs::Result<()> {
     // Encode image and save it into file.
     let lib_heif = LibHeif::new();
     let mut context = HeifContext::new()?;
@@ -45,9 +47,33 @@ fn encode2() -> libheif_rs::Result<()> {
         CompressionFormat::Av1,
     )?;
     encoder.set_quality(EncoderQuality::LossLess)?;
-    context.encode_image(&image, &mut encoder, None)?;
 
+    // Metadata lives on image 0 / primary image, see
+    // https://github.com/rlxone/Equinox/blob/43ad99abb45a027048a1bcd32d99a6b5468a234a/EquinoxCore/EquinoxCore/Cores/ImageCore.swift#L71
+    // TODO: add metadata here, see context.add_{}_metadata()
+
+    // metadata from here:
+    // https://github.com/rlxone/Equinox/blob/main/EquinoxCore/EquinoxCore/Cores/MetadataCore.swift#L57
+
+    // time is encoded as a fraction of the seconds since midnight I guess, e.g. for 1am it's 1/24 = 0.04166667
+    // encoded as pairs of "i" (index, probably image index) and "t" (time)
+
+    // unclear if we also have to encode an "appearance" (night / day)
+    // and explicitly code somewhere that we care about time, i.e. ImageMetadata=ti
+    // encoded as a property list, and then apparently converted to base64
+    // and stuffed into a metadata field
+    // and then there's a bunch of extra wrapping content
+    // Can use https://crates.io/crates/plist to build the plist
+
+    for hour in 0..24 {
+        let path = format!("output/win98_{:02}_00.png", hour);
+        let frame = frame_from_path(&path).unwrap();
+        let imgHandle = context.encode_image(&frame, &mut encoder, None)?;
+        println!("Done {}/24", hour + 1);
+    }
+    println!("Finalizing...");
     context.write_to_file("output.heif")?;
+    println!("Done!");
 
     Ok(())
 }
