069a32279ac5ed540dfb313adac13c2d4553b708
Fabian Tamp <fabian.tamp@gmail.com>
2025-02-01 13:42:25 +0100

    Cleanup code a little

--- src/draw/main.rs
+++ src/draw/main.rs
@@ -7,14 +7,11 @@ use lazy_static::lazy_static;
 
 fn render_taskbar(
     mut img: &mut PhotonImage,
-    taskbar_left: &PhotonImage,
-    taskbar_mid: &PhotonImage,
-    taskbar_right: &PhotonImage,
 ) {
     let heights = [
-        taskbar_left.get_height(),
-        taskbar_right.get_height(),
-        taskbar_mid.get_height(),
+        TASKBAR_LEFT.get_height(),
+        TASKBAR_RIGHT.get_height(),
+        TASKBAR_MID.get_height(),
     ];
     let taskbar_height = heights
         .iter()
@@ -26,20 +23,19 @@ fn render_taskbar(
         })
         .unwrap();
 
-    // 1: drop the taskbar_left
-
+    // 1: draw the taskbar_left
     let img_height = img.get_height();
     let mut x_pos = 0;
-    let start_right = img.get_width() - taskbar_right.get_width();
-    let taskbar_mid_width = taskbar_mid.get_width();
+    let start_right = img.get_width() - TASKBAR_RIGHT.get_width();
+    let taskbar_mid_width = TASKBAR_MID.get_width();
     let taskbar_y = img_height - taskbar_height;
 
-    multiple::watermark(&mut img, &taskbar_left, x_pos, taskbar_y);
-    x_pos += taskbar_left.get_width();
+    multiple::watermark(&mut img, &TASKBAR_LEFT, x_pos, taskbar_y);
+    x_pos += TASKBAR_LEFT.get_width();
 
     // 2: fill the mid
     loop {
-        multiple::watermark(&mut img, &taskbar_mid, x_pos, taskbar_y);
+        multiple::watermark(&mut img, &TASKBAR_MID, x_pos, taskbar_y);
 
         x_pos += taskbar_mid_width;
         if x_pos >= start_right {
@@ -48,7 +44,7 @@ fn render_taskbar(
     }
 
     // 3: draw the right
-    multiple::watermark(&mut img, &taskbar_right, start_right, taskbar_y);
+    multiple::watermark(&mut img, &TASKBAR_RIGHT, start_right, taskbar_y);
 }
 
 fn bytes_for_digit(digit: u8) -> &'static[u8] {
@@ -91,24 +87,32 @@ lazy_static! {
     static ref SYMBOL_COLON: PhotonImage = scale(&load_res_image!("colon.png"));
     static ref SYMBOL_AM: PhotonImage = scale(&load_res_image!("am.png"));
     static ref SYMBOL_PM: PhotonImage = scale(&load_res_image!("pm.png"));
+
+    static ref TASKBAR_LEFT: PhotonImage = scale(&load_res_image!("taskbar-start.png"));
+    static ref TASKBAR_MID: PhotonImage = scale(&load_res_image!("taskbar-mid.png"));
+    static ref TASKBAR_RIGHT: PhotonImage = scale(&load_res_image!("taskbar-end.png"));
 }
 
 
 fn render_clock(mut img: &mut PhotonImage, clock_pos: (u32, u32), hour: u8, min: u8) {
     assert!(hour < 24);
+    assert!(min < 60);
+
     let pm  = (hour / 12) != 0;
     let two_digit_hour = (hour % 12) == 0 || (hour % 12) == 11;
+
     let mut x_pos = clock_pos.0;
     let y_pos = clock_pos.1;
     if two_digit_hour {
-        // TODO this shouldn't be hardcoded in this method
+        // Shift left by three pixels (unscaled) if there's a second digit
         x_pos -= 3 * TASKBAR_SCALE;
     }
     if two_digit_hour {
+        // it's always a one
         multiple::watermark(&mut img, &DIGITS[1], x_pos, y_pos);
         x_pos += DIGITS[1].get_width() + TASKBAR_SCALE;
     }
-    // 12:00 -> 12
+
     let second_digit = if hour == 12 || hour == 0 {
         2
     } else {
@@ -131,7 +135,6 @@ fn render_clock(mut img: &mut PhotonImage, clock_pos: (u32, u32), hour: u8, min:
     x_pos += digit_img.get_width() + TASKBAR_SCALE;
 
     // AM / PM
-    // TODO fix scaling, this should probably be 2 * TASKBAR_SCALE
     x_pos += 2 * TASKBAR_SCALE;
 
     if pm {
@@ -150,34 +153,20 @@ const TASKBAR_SCALE: u32 = 2;
 
 fn main() {
     let bkg_png = include_bytes!("../../res/bkg.png");
-    let taskbar_left_bytes = include_bytes!("../../res/taskbar-start.png");
-    let taskbar_mid_bytes = include_bytes!("../../res/taskbar-mid.png");
-    let taskbar_right_bytes = include_bytes!("../../res/taskbar-end.png");
     // relative to taskbar_right's origin, prior to scaling
     let clock_offset = (54, 11);
 
     let mut img = open_image_from_bytes(bkg_png).expect("File should open");
-    let taskbar_left = open_image_from_bytes(taskbar_left_bytes).expect("File should open");
-    let taskbar_mid = open_image_from_bytes(taskbar_mid_bytes).expect("File should open");
-    let taskbar_right = open_image_from_bytes(taskbar_right_bytes).expect("File should open");
-
-    let taskbar_left = scale(&taskbar_left);
-    let taskbar_mid = scale(&taskbar_mid);
-    let taskbar_right = scale(&taskbar_right);
     let clock_offset = (clock_offset.0 * TASKBAR_SCALE, clock_offset.1 * TASKBAR_SCALE);
 
-    let taskbar_right_offset_x = img.get_width() - taskbar_right.get_width();
-    let taskbar_right_offset_y = img.get_height() - taskbar_right.get_height();
+    let taskbar_right_offset_x = img.get_width() - TASKBAR_RIGHT.get_width();
+    let taskbar_right_offset_y = img.get_height() - TASKBAR_RIGHT.get_height();
     let clock_pos = (taskbar_right_offset_x + clock_offset.0, taskbar_right_offset_y + clock_offset.1);
 
-    render_taskbar(&mut img, &taskbar_left, &taskbar_mid, &taskbar_right);
+    render_taskbar(&mut img);
     render_clock(&mut img, clock_pos, 13, 00);
 
     save_image(img, "single.png").expect("Save failed");
-
-    // TODO: this is calculated in multiple places, sort it out
-    // let taskbar_top = bkg_img.get_height() - taskbar_left.get_height();
-    // let taskbar_right_start = bkg_img.get_width() - taskbar_right.get_width();
 
     // (0..24).into_par_iter().for_each(|hour| {
     //     for min in 0..60 {
